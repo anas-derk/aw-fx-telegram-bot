d5561a768aebe8eda09e2a1b5103abbc6f60a989
anas derk <anas.derk2020@gmail.com>
2024-01-19 10:41:44 -0800

    Deploy Telegram Bot

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.telegramBotToken, { polling: true });
+
+const channelIds = [
+    "@ebnelahdall",
+    "@metaverse00",
+];
+
+bot.onText(/\/start/, async (msg) => {
+    try{
+        await bot.sendMessage(msg.chat.id, "مرحباً بك في البوت الخاص بنا");
+    }
+    catch(err) {
+        throw Error(err);
+    }
+});
+
+bot.on("message", async (msg) => {
+    try{
+        if (msg.from.id === parseInt(process.env.adminId)) {
+            if (msg.text !== "/start") {
+                for(let i = 0; i < channelIds.length; i++) {
+                    await bot.sendMessage(channelIds[i], msg.text);
+                }
+            }
+        } else await bot.sendMessage(msg.chat.id, "عذراً أنت لا تمتلك الصلاحية لكتابة منشور في  البوت");
+    }
+    catch(err) {
+        throw Error(err);
+    }
+});
